d7e6d49dd884d95fc1f4a16a63f7ba3459948c28
Rafiere <rafapgbr@gmail.com>
2023-05-13 10:36:49 -0300

    feat: add vehicle entity

--- src/main/java/com/expo/springdatajpaandhibernategeneralconcepts/car/model/VehicleType.java
+++
@@ -1,6 +0,0 @@
-package com.expo.springdatajpaandhibernategeneralconcepts.car.model;
-
-public enum VehicleType {
-
-	MOTORCYCLE, CAR, TRUCK, BUS
-}
---
+++ src/main/java/com/expo/springdatajpaandhibernategeneralconcepts/vehicle/model/VehicleType.java
@@ -0,0 +1,6 @@
+package com.expo.springdatajpaandhibernategeneralconcepts.vehicle.model;
+
+public enum VehicleType {
+
+	MOTORCYCLE, CAR, TRUCK, BUS
+}
